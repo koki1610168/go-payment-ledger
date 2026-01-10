f1255dee04936fb8de2c6f37c1f633c370b61a60
koki1610168 <k.yahata1610168@gmail.com>
2026-01-10 09:42:24 +0900

    adding handler and its test

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -3,8 +3,13 @@ package server
 import (
 	"context"
 	"net/http"
-	"fmt"
+	"strings"
+	"encoding/json"
 )
+type BalanceResponse struct {
+	Balance int
+	Currency string
+}
 
 type ClientStore interface {
 	GetBalance(ctx context.Context, clientId string) (int64, string, error) 
@@ -29,10 +34,16 @@ func NewHandler(store ClientStore) *Handler{
 
 
 func (h *Handler) postPayments(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(http.StatusOK)
 
 }
 
 func (h *Handler) getBalance(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(http.StatusOK)
+	// We want to call GetBalance
+	client_id := strings.TrimPrefix(r.URL.Path, "/clients/")
+	client_id = strings.TrimSuffix(client_id, "/balance")
+
+	balance, currency, _ := h.store.GetBalance(r.Context(), client_id)
+
+	json.NewEncoder(w).Encode(BalanceResponse{int(balance), currency})
+
 }
--- internal/server/handler_test.go
+++ internal/server/handler_test.go
@@ -1,14 +1,17 @@
 package server
 
 import (
+	"fmt"
 	"testing"
 	"net/http"
 	"net/http/httptest"
 	"context"
+	"encoding/json"
 )
 
 
 type StubStore struct {
+
 
 }
 
@@ -21,14 +24,28 @@ func (s *StubStore) CreatePayment(ctx context.Context, clientID string, amount i
 }
 
 func TestHandler(t *testing.T) {
+	// The request should be json and the resonse is also json
+	// I want to make a fake dateabase
 	t.Run("Initial setup ", func(t *testing.T) {
-		s := StubStore{}
-		handler := NewHandler(&s)
+		_, _, store  := newTestStore(t)
 
-		request, _ := http.NewRequest(http.MethodPost, "/payments", nil)
+		handler := NewHandler(store)
+
+		request, _ := http.NewRequest(http.MethodGet, "/clients/client_001/balance", nil)
 		response := httptest.NewRecorder()
 
-		handler.postPayments(response, request)
+		//handler.postPayments(response, request)
 		handler.getBalance(response, request)
+
+		var balanceClient BalanceResponse
+		err := json.NewDecoder(response.Body).Decode(&balanceClient)
+		if err != nil {
+			t.Errorf("error occured")
+		}
+		fmt.Println(balanceClient.Balance)
+
 	})
+	
+
 }
+
